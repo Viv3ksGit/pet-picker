170af3e61acbdc57cdaa2a7739d32c9b66e367fb
jeffreyneildsouza <jeffrey.neil.dsouza@gmail.com>
2019-07-26 17:11:14 -0400

    updated favorites binding

--- PuppyPicker/ViewModels/FavoritesPageVM.cs
+++ PuppyPicker/ViewModels/FavoritesPageVM.cs
@@ -4,15 +4,22 @@ namespace PuppyPicker.ViewModels
 {
     public class FavoritesPageVM : BaseVM
     {
+        public string FP_Title { get; set; }
+
 
         public FavoritesPageVM()
         {
-
+            ;
         }
 
         public string ImageFile
         {
             get { return string.Format("pug.jpeg"); }
+        }
+
+        public string DogName
+        {
+            get { return string.Format("Dog Name"); }
         }
     }
 }
