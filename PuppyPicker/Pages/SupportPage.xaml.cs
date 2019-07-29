2dfa2993d9bc1d61072bb90f229f604005357e05
Vivek <Viv3kdev@gmail.com>
2019-07-28 21:04:34 -0400

    Added functionality for Suggestion/Feedback button

--- PuppyPicker/Pages/SupportPage.xaml.cs
+++ PuppyPicker/Pages/SupportPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
+using Plugin.Messaging;
 using Xamarin.Forms;
 
 namespace PuppyPicker
@@ -11,5 +12,23 @@ namespace PuppyPicker
         {
             InitializeComponent();
         }
+
+        void Support_Clicked(object sender, System.EventArgs e)
+        {
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+              
+                var email = new EmailMessageBuilder()
+                  .To("to.Vivekmohan.rhul@gmail.com")
+                  .Subject("Suggestion/Feedback")
+                  .Body("Hello Developer")
+                  .Build();
+
+                emailMessenger.SendEmail(email);
+            }
+        }
+
+        }
     }
-}
+
