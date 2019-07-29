f4f07db9fbce8e9f5f139fd1c9523e5eb6e0f81d
Vivek <Viv3kdev@gmail.com>
2019-07-29 17:37:19 -0400

    Added binding for support page

--- PuppyPicker/Pages/SupportPage.xaml.cs
+++ PuppyPicker/Pages/SupportPage.xaml.cs
@@ -1,54 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Plugin.Messaging;
-using Xamarin.Essentials;
+using System.Net.Http;
+using PuppyPicker.ENUMS;
+using System.Text;
 using Xamarin.Forms;
+using Plugin.Toast;
+using PuppyPicker.Classes;
+using PuppyPicker.BaseClasses;
+
 
 namespace PuppyPicker
 {
-    public partial class SupportPage : ContentPage
+    public partial class SupportPage : BaseClasses.BasePage
     {
         public SupportPage()
         {
             InitializeComponent();
         }
-
-        void Support_Clicked(object sender, System.EventArgs e)
-        {
-            var emailMessenger = CrossMessaging.Current.EmailMessenger;
-            if (emailMessenger.CanSendEmail)
-            {
-
-                var email = new EmailMessageBuilder()
-                  .To("to.Vivekmohan.rhul@gmail.com")
-                  .Subject("Suggestion/Feedback")
-                  .Body("Hello Developer")
-                  .Build();
-
-                emailMessenger.SendEmail(email);
-            }
-
-            else
-            {
-                DisplayAlert("Application not found", "Please install application to use this service", "OK");
-
-            }
-
-           
-        }
-
-        void Call_Clicked(object sender, System.EventArgs e)
-        {
-            var phoneDialer = CrossMessaging.Current.PhoneDialer;
-            if (phoneDialer.CanMakePhoneCall)
-                phoneDialer.MakePhoneCall("+27219333000");
-            else
-            {
-                DisplayAlert("Call function not found", "Please install application to use this service", "OK");
-            }
-        }
     }
 }
-
-
--- PuppyPicker/ViewModels/SupportPageVM.cs
+++ PuppyPicker/ViewModels/SupportPageVM.cs
@@ -1,13 +1,58 @@
 ﻿using System;
+using System.Windows.Input;
+using Plugin.Messaging;
 using PuppyPicker.BaseClasses;
+using Xamarin.Forms;
 
 namespace PuppyPicker.ViewModels
 {
     public class SupportPageVM : BaseVM
     {
+        public string SSP_Title { get; set; }
+
+        public ICommand Email_Clicked => new Command(EmailClicked);
+        public ICommand Call_Clicked => new Command(CallClicked);
+
         public SupportPageVM()
         {
-            //MyApp.MainPage.DisplayAlert("", "Password requirments not fullfilled", "OK");
+            SSP_Title = "Support";
         }
+
+        private void CallClicked(object obj)
+        {
+
+            var phoneDialer = CrossMessaging.Current.PhoneDialer;
+            if (phoneDialer.CanMakePhoneCall)
+                phoneDialer.MakePhoneCall("+27219333000");
+            else
+            {
+                MyApp.MainPage.DisplayAlert("Call function not found", "Please install application to use this service", "OK");
+            }
+        }
+
+
+            private void EmailClicked(object obj)
+
+            {
+                var emailMessenger = CrossMessaging.Current.EmailMessenger;
+                if (emailMessenger.CanSendEmail)
+                {
+
+                    var email = new EmailMessageBuilder()
+                      .To("to.Vivekmohan.rhul@gmail.com")
+                      .Subject("Suggestion/Feedback")
+                      .Body("Hello Developer")
+                      .Build();
+
+                    emailMessenger.SendEmail(email);
+                }
+
+                else
+                {
+                   MyApp.MainPage.DisplayAlert("Application not found", "Please install application to use this service", "OK");
+
+                }
+
+            }
     }
 }
